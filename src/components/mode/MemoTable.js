fe4728172cdd0a7f783f29d87369878ad9299bf6
lala-p <sangmicell@gmail.com>
2021-11-01 10:29:49 +0900

    memoTable map 구조
    (cherry picked from commit aae3688c357f0b81197d25438caafe65961b853f)

--- src/components/mode/MemoTable.js
+++ src/components/mode/MemoTable.js
@@ -1,208 +1,133 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 
 const MemoBoard = () => {
 
-    const [test, setTest] = useState('');
-
-    const dispatch = useDispatch();
     const mainText = useSelector((state) => state.mainText.mainText);
-
-    // const [mainText, setMainText] = useState("")
+    const [noneLineBreakText, setNoneLineBreakText] = useState("")
 
     const [week, setWeek] = useState([])
-    // const [day, setDay]   = useState([])
-    const [plan, setPlan] = useState([])
-    const [etc, setEtc]   = useState([])
-    
-    const [memo, setMemo] = useState([]) 
     
 
+    const weekBox = (text) =>{
+        const box = text.map((thatWeek) => {
 
-    const planBox = (dayOfPlan) => {
-        const box = dayOfPlan.map((plan) => (
-            <div>
-                
-            
-            </div>
-        ))
+            let memo = []
+            memo = thatWeek.split(/\-{35}/g)
+            memo.pop()
+
+            return (
+                <div>
+                    <div style={{display: "flex", width: "1350px", backgroundColor: "green"}}>
+                        {memoBox(memo)}
+                    </div>
+                    <hr />
+                    <br />
+                </div>
+            )
+        })
+
         return box;
     }
 
-
-    // const dayBox = day.map((dayBox) => (
-    //     <div>
-            
-
-
-    //     </div>
-    // ))
 
     const memoBox = (dayOfMemo) => {
-        const box = dayOfMemo.map((memo) => (
-            <div style={{width: "260px", height: "150px", backgroundColor: "gray", margin: "5px"}}>
-                {memo}
-            </div>            
-        ))
+        const box = dayOfMemo.map((memo) => {
+
+            let text = memo
+            
+            let date = text.match(/={3}\s\d{4}\/\d{2}\/\d{2}\s\={20}/g)
+            date = date[0].replace(/(=|\s)/g, "")
+            text = text.replace(/={3}\s\d{4}\/\d{2}\/\d{2}\s\={20}/g)
+
+            let etc = text.split(/\+{1}/g)
+            etc.shift()
+            for (let index = 0; index < etc.length; index++) {
+                text = text.replace('+' + etc[index], "")
+            }
+
+            let plan = text.split(/\d{1}\.{1}/g)
+            plan.shift()
+
+            return (
+                <div style={{width: "260px", height: "150px", backgroundColor: "gray", margin: "5px"}}>
+                    <b>{date}</b>
+                    <br />
+                    <ol>
+                        {planBox(plan)}
+
+                    </ol>
+                    <br /><br /> 
+                    {etcBox(etc)}                   
+                </div>            
+            )
+        })
 
         return box;
     }
 
-    const gg = [1,2,3,4]
+    const planBox = (dayOfPlan) => {
+        const box = dayOfPlan.map((plan) => {
+            return (
+                <li>
+                    {plan}
+                </li>
+            ) 
+        })
+        
+        return box;
+    }
 
-    const weekBox = week.map((thatWeek) => (
-        <div>
-            <div style={{display: "flex", width: "1350px", backgroundColor: "green"}}>
-                {/* <div style={{width: "285px", height: "150px", backgroundColor: "gray", marginLeft: "20px"}}></div>
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray", marginLeft: "20px"}}></div>
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray", marginLeft: "20px"}}></div>
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray", marginLeft: "20px"}}></div>
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray", marginLeft: "20px"}}></div> */}
-                {memoBox(thatWeek.memo)}
-            </div>
-            <hr />
-            <br />
-        </div>
-    ))
+    const etcBox = (dayOfEtc) => {
+        const box = dayOfEtc.map((etc) => {
+            return (
+                <div>
+                    {"+ " + etc}
+                </div>
+            )
+        })
+
+        return box;
+    }
 
    
-    const [count, setCount] = useState(0)
     useEffect(() => {
-        // 일주일마다 나누고
-        // memo 만드는 것까지 
         
         let text = mainText
         text = text.replace(/\n/g,"")
-        const weekLine = /(?<=-{35})-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-(?=\={3}\s\d{4}\/\d{2}\/\d{2}\s\={20})/g
-        let weekArr = []
-
-        if (text.length != 0) {
-            weekArr = text.split(weekLine)
-            // console.log(weekArr)
-        }
-
-        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-        let bb = []
-        // let bbCount = 0
-
-        for (let index = 0; index < weekArr.length; index++) {
-            let memo = weekArr[index].split(/\-{35}/g)
-            memo.pop()
-            bb.push({
-                memo: memo
-            })
-            // bbCount += yy.length
-        }
-
-        console.log(bb.length)
-        
-        setCount(text)
-        setWeek(bb)
-
+        setNoneLineBreakText(text)
+    
     }, [mainText])
 
 
     useEffect(() => {
 
+        const weekLine = /(?<=-{35})-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-(?=\={3}\s\d{4}\/\d{2}\/\d{2}\s\={20})/g
+        let weekArr = []
 
-        let day = []
-        let bb = []
-
-        for (let index = 0; index < week.length; index++) {
-            day = week[index]['memo']
-
-
+        if (noneLineBreakText.length != 0) {
+            weekArr = noneLineBreakText.split(weekLine)
         }
 
-        console.log("count")
-        // setWeek(bb)
-    }, [count])
+        setWeek(weekArr)
+
+    }, [noneLineBreakText])
 
 
     return(
         <div>
-            {/* <div>
-                <textarea value={mainText} onChange={(e)=>setMainText(e.target.value)}></textarea>
-            </div> */}
-                {/* {test}
-            <button onClick={()=> setTest('asdfasdfadfsfdasdf')}>click</button> 
-            useState 값은 mode가 변경되면 초기화됨. */}
-            <br /><br />
+            
             <br />
 
             <div style={{width: "1370px", height: "900px", overflow: "scroll", margin: "auto"}}>
             { week.length == 0 ? 
                 <div>???</div>
                 : 
-                weekBox
+                weekBox(week)
             }
 
-
             </div>
-
-
-            {/* <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-            <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-            <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-            <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp; */}
-
-            {/* <div style={{display: "flex"}}>
-                    
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                
-            </div>
-            <hr />
-            <br />
-            <div style={{display: "flex"}}>
-                
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                
-            </div>
-            <hr />
-            <br />
-            <div style={{display: "flex"}}>
-                
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                
-            </div>
-            <hr />
-
-            <br />
-            <div style={{display: "flex"}}>
-                
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                
-            </div>
-            <hr />
-
-            <br />
-            <div style={{display: "flex"}}>
-                
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                <div style={{width: "285px", height: "150px", backgroundColor: "gray"}}>vcxzcvzcv</div> &nbsp;&nbsp;&nbsp;
-                
-            </div>
-            <hr /> */}
         </div>
     )
 
